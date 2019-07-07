057f0e0ba1aca83dcf6b13b642f0e93549cfb3b6
thegergo02 <thegergo02@tutanota.com>
2019-07-07 10:06:37 +0200

    cogs: delete old files

--- command.py
+++
@@ -1,7 +0,0 @@
-import discord
-class command(object):
-    func = ""
-    perms = ""
-    def __init__(self, func, perms=discord.Permissions(permissions=0)):
-        self.func = func
-        self.perms = perms
--- music_object.py
+++
@@ -1,7 +0,0 @@
-
-class music_object(object):
-    def __init__(self, id, title, uploader, url):
-        self.id = id
-        self.title = title
-        self.uploader = uploader
-        self.url = url
