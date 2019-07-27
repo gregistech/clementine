a3a797ccf19d8c6915a437eb3a6e53087d909a8e
thegergo02 <thegergo02@tutanota.com>
2019-07-27 16:25:55 +0200

    refactor: use typing.Optional

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,11 +1,12 @@
 import discord
+import typing
 from discord.ext import commands
 
 class ModCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command(name='purge')
     @commands.has_role('Moderators')
     async def purge(self, ctx, *, limit:int):
@@ -13,17 +14,17 @@ class ModCog(commands.Cog):
     
     @commands.command(name='kick')
     @commands.has_role('Moderators')
-    async def kick(self, ctx, member:discord.Member, *, reason:str=None):
+    async def kick(self, ctx, member:discord.Member, *, reason:typing.Optional[str]=None):
         await member.kick(reason=reason)
     
     @commands.command(name='ban')
     @commands.has_role('Moderators')
-    async def ban(self, ctx, member:discord.Member, *, reason:str=None):
+    async def ban(self, ctx, member:discord.Member, *, reason:typing.Optional[str]=None):
         await member.ban(reason=reason)
     
     @commands.command(name='unban')
     @commands.has_role('Moderators')
-    async def unban(self, ctx, mention:str, *, reason:str=None):
+    async def unban(self, ctx, mention:str, *, reason:typing.Optional[str]=None):
         member = discord.Object(id=mention.replace('<', '').replace('>', '').replace('@', ''))
         await ctx.guild.unban(member, reason=reason)
     
